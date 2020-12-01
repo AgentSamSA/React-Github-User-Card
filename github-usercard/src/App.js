69f2066abc3fe48d44041f88c37f516ba84d1138
Samuel Lee <samuelxlee@gmail.com>
2020-12-01 14:45:42 -0800

    added api call via axios to grab user information and render inside browser, as well as search feature to get different users

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,12 +1,89 @@
 import React from "react";
+import fetchUserData from "./services/fetchUserData";
+import { initialUser } from "./services/constants";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    userInfo: {},
+    userFollowerInfo: [],
+    searchInput: ""
+  }
 
-    </div>
-  );
+  componentDidMount() {
+    fetchUserData(initialUser)
+      .then((res) => {
+        this.setState({
+          userInfo: res.data,
+          userFollowerInfo: res.data.followers_url
+        });
+      });
+  }
+
+  onChange = (event) => {
+    this.setState({
+      searchInput: event.target.value
+    });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.searchInput);
+    fetchUserData(this.state.searchInput)
+      .then((res) => {
+        this.setState({
+          userInfo: res.data,
+          userFollowerInfo: res.data.followers_url
+        })
+      });
+  }
+
+  render() {
+    let userState = this.state.userInfo;
+    let user = userState.login;
+    let userFollowerState = this.state.userFollowerInfo;
+
+    return (
+      <div>
+        <h1>Get a Github User's Card!</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <input
+            value={this.state.searchInput}
+            placeholder="Search for a user"
+            onChange={this.onChange} />
+          <button>Search</button>
+        </form>
+
+        <div>
+          <h2>{user}</h2>
+
+          <img width="300" src={userState.avatar_url} alt={user} />
+          <br></br>
+
+          <a href={userState.html_url} target="_blank">{`${user}'s Github Profile`}</a>
+
+          <p>Following: {userState.following}</p>
+
+          <p>Followed by: </p>
+
+          <div>
+            {
+              !userFollowerState ? "Loading follower information" :
+              userFollowerState.map(follower => {
+                return (
+                  <div key={follower.id}>
+                    <h4>{follower.login}</h4>
+                    <img width="150" src={follower.avatar_url} alt={follower.login} />
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
--- github-usercard/src/services/fetchUserData.js
+++ github-usercard/src/services/fetchUserData.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-import { API_LINK } from "constants";
+import { API_LINK } from "./constants";
 
 const fetchUserData = (user) => {
     return axios.get(API_LINK + user)
     .then((res) => {
+        console.log(res.data);
         return (res);
     })
     .catch(err => console.log(err));
